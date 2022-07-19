cd34b43211bbc711dbb8b8aa2e0840d2e743c663
Sam Smith <romanmissilin@mail.ru>
2022-07-19 09:09:00 +0300

    Готово

--- src/main/java/com/example/homework_2_11/element/Basket.java
+++ src/main/java/com/example/homework_2_11/element/Basket.java
@@ -2,13 +2,13 @@ package com.example.homework_2_11.element;
 
 import com.example.homework_2_11.item.Item;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@RequestScope
+@SessionScope
 public class Basket {
 
     private final List<Item> items = new ArrayList<>();
